e6cfc17d2a29e32a7d37a00daa1021c710865b0d
Meghane Saidenberg-Coriston <msaidenberg@uchicago.edu>
2024-11-18 15:49:27 -0600

    adapted submission script

--- scripts/train_batch_dsi.sh
+++ scripts/train_batch_dsi.sh
@@ -9,9 +9,9 @@
 #SBATCH --open-mode=append # So that outcomes are appended, not rewritten
 #SBATCH --signal=SIGUSR1@90
 
-export NUGRAPH_DIR=/net/projects/fermi-gnn/24autumn/meghane/next_try/2024-autumn-nugraph/nugraph/nugraph
+export NUGRAPH_DIR=/net/projects/fermi-gnn/24autumn/meghane/meghane_edits/2024-autumn-nugraph/nugraph/nugraph
 export NUGRAPH_DATA=/net/projects/fermi-gnn/24summer/helal/preprocessing3/graph.h5
-export NUGRAPH_LOG=/net/projects/fermi-gnn/24autumn/meghane/next_try/2024-autumn-nugraph/checkpoints
+export NUGRAPH_LOG=/net/projects/fermi-gnn/24autumn/meghane/meghane_edits/2024-autumn-nugraph/checkpoints
 
 export WANDB_API_KEY=4664589d68421aad612bfac0a95ff1d73ef2312b
 
