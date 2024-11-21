b357d8bdf6665382f6a9f06d6e8675a6be2607e3
Edward Gu <ygu0929@uchicago.edu>
2024-11-20 22:31:27 -0600

    Full training script for DSI cluster usage

--- scripts/train_batch_dsi.sh
+++ scripts/train_batch_dsi.sh
@@ -5,8 +5,27 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=8
-#SBATCH --mem-per-cpu=30G
+#SBATCH --mail-user=ygu0929@uchicago.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mem-per-cpu=32G
 #SBATCH --open-mode=append # So that outcomes are appended, not rewritten
 #SBATCH --signal=SIGUSR1@90
 
-srun python scripts/train.py $@
+
+# If there are existing checkpoint files, specify ckpt_dir
+ckpt_dir=/home/ygu0929/dsi24-merge/2024-autumn-nugraph/checkpoints/
+files=$(ls -t $ckpt_dir)
+latest_file=$(echo "$files" | head -n 1) # Get the first (latest) file
+ckpt_path=$ckpt_dir$latest_file # Construct the full path to the most recent checkpoint file
+echo $ckpt_path # This will print ckpt_path in .out file
+
+# Add either of these two flag combinations to srun command below
+# Choose based on if you have ckpt files or not
+# Choice 1: --logdir /net/projects/fermi-gnn/%u --name vertex
+# Choice 2: --resume $ckpt_path
+srun python /home/ygu0929/dsi24-merge/2024-autumn-nugraph/scripts/train.py --name pmt --version 24.7.1 --data-path /net/projects/fermi-gnn/24summer/helal/preprocessing3/graph.h5 --semantic --filter --logdir /home/ygu0929/dsi24-merge/2024-autumn-nugraph/checkpoints/ --name pmt
+# /net/projects/fermi-gnn/CHEP2023.gnn.h5
+# /net/projects/fermi-gnn/24autumn/edward/24summer_replication/24summer/ashmit/preprocessing3/graph.h5
+# This is an examble submission command
+# You submit through the terminal after activating numl-dsi
+# sbatch /net/projects/fermi-gnn/24summer/ashmit/NuGraph/scripts/train_batch_dsi.sh --version attentional-mlp-64-sementic-filter --semantic --filter
