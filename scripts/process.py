b59620f261ade2ff391cafe02564a3f49a75f96e
Meghane Saidenberg-Coriston <msaidenberg@uchicago.edu>
2025-03-10 11:55:23 -0500

    this has the new processing connections info

--- scripts/process.py
+++ scripts/process.py
@@ -2,27 +2,40 @@
 import argparse
 import pynuml
 
+
 def configure():
     args = argparse.ArgumentParser()
-    args.add_argument("-i", "--infile", type=str, required=True,
-                      help="input HDF5 file")
-    args.add_argument("-o", "--outfile", type=str, required=True,
-                      help="output HDF5 file pattern")
-    args.add_argument('--label-vertex', action='store_true', default=False,
-                      help='add true vertex label to graphs')
-    return args.parse_args()  
+    args.add_argument("-i", "--infile", type=str, required=True, help="input HDF5 file")
+    args.add_argument(
+        "-o", "--outfile", type=str, required=True, help="output HDF5 file pattern"
+    )
+    args.add_argument(
+        "--label-vertex",
+        action="store_true",
+        default=False,
+        help="add true vertex label to graphs",
+    )
+    args.add_argument(
+        "--connections-dev",
+        action="store_true",
+        default=False,
+        help="add intermediate connections between opflashsumpe and nexus",
+    )
+    return args.parse_args()
+
 
 def process(args):
-
     # open input file
     f = pynuml.io.File(args.infile)
 
     # create graph processor
     processor = pynuml.process.HitGraphProducer(
-            file=f,
-            semantic_labeller=pynuml.labels.SimpleLabels(),
-            event_labeller=pynuml.labels.FlavorLabels(),
-            label_vertex=args.label_vertex)
+        file=f,
+        semantic_labeller=pynuml.labels.SimpleLabels(),
+        event_labeller=pynuml.labels.FlavorLabels(),
+        label_vertex=args.label_vertex,
+        connections_dev=args.connections_dev,
+    )
 
     # create output file stream
     out = pynuml.io.H5Out(args.outfile)
@@ -30,6 +43,9 @@ def process(args):
     # run processing
     f.process(processor, out)
 
+
 if __name__ == "__main__":
+    print("Starting Processing")
     args = configure()
     process(args)
+    print("Finished Processing")
